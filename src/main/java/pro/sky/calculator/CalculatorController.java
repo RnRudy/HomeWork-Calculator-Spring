2a921ab20499fdf8013423d585bc974285c94489
Nikita.Rudy <NVRudy@gmail.com>
2023-10-14 14:45:44 +0300

    Calculator

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
+    public boolean RequestParam;
 
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
@@ -16,21 +17,23 @@ public class CalculatorController {
     }
 
     @RequestMapping(path = "/plus")
-    public String calculatingPlus(@RequestParam("num1") int num1, @RequestParam ("num2") int num2) {
-        return num1 + " + " + num2 + " = " + calculatorService.calculatingPlus(num1,num2);    }
+    public String calculatingPlus(@RequestParam(value = "num1", required = false) int num1, @RequestParam (value = "num2",required = false) int num2) {
+           return num1 + " + " + num2 + " = " + calculatorService.calculatingPlus(num1, num2);
+        }
+
 
     @RequestMapping(path = "/minus")
-    public String calculatingMinus(@RequestParam("num1") int num1,@RequestParam("num2") int num2) {
+    public String calculatingMinus(@RequestParam(value = "num1", required = false) int num1,@RequestParam(value = "num2", required = false) int num2) {
         return num1 + " - " + num2 + " = " + calculatorService.calculatingMinus(num1,num2);    }
 
 
     @RequestMapping(path = "/multiply")
-    public String calculatingMultiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+    public String calculatingMultiply(@RequestParam(value = "num1", required = false) int num1, @RequestParam(value = "num2", required = false) int num2) {
         return num1 + " * " + num2 + " = " + calculatorService.calculatingMultiply(num1,num2);    }
 
 
     @RequestMapping(path = "/divide")
-    public String calculatingDivide(@RequestParam("num1") int num1, @RequestParam ("num2") int num2) {
+    public String calculatingDivide(@RequestParam(value = "num1", required = false) int num1, @RequestParam (value = "num2", required = false) int num2) {
         return num1 + " / " + num2 + " = " + calculatorService.calculatingDivide(num1,num2);
     }
 
--- src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -10,24 +10,29 @@ public class CalculatorServiceImpl implements CalculatorService {
     private String zero;
 
     public String divideByZero(String zero) {
-      return   "Мы не делим на ноль!";
+        return "Мы не делим на ноль!";
     }
-    public int calculatingPlus(int num1, int num2) {
+
+    public Serializable calculatingPlus(int num1, int num2) {
         return num1 + num2;
     }
 
-    public int calculatingMinus(int num1, int num2) {
+    public Serializable calculatingMinus(int num1, int num2) {
+
         return num1 - num2;
     }
 
-    public int calculatingMultiply(int num1, int num2) {
+    public Serializable calculatingMultiply(int num1, int num2) {
+
         return num1 * num2;
     }
+
     public Serializable calculatingDivide(int num1, int num2) {
         if (num1 != 0 && num2 != 0) {
-            return num1 / num2 ;
+            return num1 / num2;
         } else {
-            return divideByZero(zero) ;
+            return divideByZero(zero);
         }
     }
 }
+
